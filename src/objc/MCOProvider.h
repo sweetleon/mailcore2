bc31f643b999db6ef909d27706b8aa6700710047
Pushkar Singh <pushkar@hothouselabs.com>
2013-05-24 00:42:28 -0700

    Added providers and fixed the load issue with NetService

---
+++ src/objc/MCOProvider.h
@@ -0,0 +1,16 @@
+//
+//  MCOProvider.h
+//  mailcore2
+//
+//  Created by Pushkar Singh on 5/23/13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#ifndef mailcore2_MCOProvider_h
+#define mailcore2_MCOProvider_h
+
+#import <MailCore/MCONetService.h>
+#import <MailCore/MCOMailProvider.h>
+#import <MailCore/MCOMailProvidersManager.h>
+
+#endif
